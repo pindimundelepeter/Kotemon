22afb3672fa8d9f67c3f03b41e70bfc94d647981
pindimundelepeter <pindimundelepeter@gmail.com>
2026-09-25 11:53:31 +0200

    Suite

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,62 @@
-package org.example
+import dresseur.Entraineur
+import monstre.EspaceMonstre.EspeceMonstre
+import monde.Zone
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/** Entraineur
+ */
+val joueur = Entraineur(1,"Peter",100)
+var rival = Entraineur(2, "Adam", 200)
+val adrien = Entraineur(3,"Adrien",300)
+val inferno = EspeceMonstre(1,"galum","Feu",700,300,500,1200,700,1500,2.5,1.5,3.5,5.5,3.4,5.2)
+val zone  = Zone(2,"Terre",23)
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    route1.zoneSuivante = route2
+    route2.zonePrecedente = route1
+    joueur.afficheDetail()
+    rival.afficheDetail()
+    adrien.afficheDetail()
+    joueur.argents+=50
+    print(inferno.afficheArt())
+    joueur.afficheDetail()
+
+
+
+
+
+
+
+    println(changeCouleur("Hello", "vert"))
+    println(changeCouleur("World","bleu"))
+    println("Hello ${changeCouleur("my","jaune")} World")
+    println(changeCouleur("Truc","marron"))
+
 }
+/**
+ * Change la couleur du message donné selon le nom de la couleur spécifié.
+ * Cette fonction utilise les codes d'échappement ANSI pour appliquer une couleur à la sortie console. Si un nom de couleur
+ * non reconnu ou une chaîne vide est fourni, aucune couleur n'est appliquée.
+ *
+ * @param message Le message auquel la couleur sera appliquée.
+ * @param couleur Le nom de la couleur à appliquer (ex: "rouge", "vert", "bleu"). Par défaut c'est une chaîne vide, ce qui n'applique aucune couleur.
+ * @return Le message coloré sous forme de chaîne, ou le même message si aucune couleur n'est appliquée.
+ */
+
+fun changeCouleur(message: String, couleur:String=""): String {
+    val reset = "\u001B[0m"
+    val codeCouleur = when (couleur.lowercase()) {
+        "rouge" -> "\u001B[31m"
+        "vert" -> "\u001B[32m"
+        "jaune" -> "\u001B[33m"
+        "bleu" -> "\u001B[34m"
+        "magenta" -> "\u001B[35m"
+        "cyan" -> "\u001B[36m"
+        "blanc" -> "\u001B[37m"
+        else -> "" // pas de couleur si non reconnu
+    }
+    return "$codeCouleur$message$reset"
+
+}
+
+
+
---
+++ src/main/kotlin/monde/Zone.kt
@@ -0,0 +1,15 @@
+package monde
+
+class Zone (
+    val id : Int,
+    var name : String,
+    var expZone : Int,
+    var zoneSuivante : Zone?,
+    var zonePrecedente : Zone?
+
+
+
+
+){
+
+}
